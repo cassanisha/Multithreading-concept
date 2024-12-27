beee67a7fcefc46012947767270e0fc76d84676c
Anisha <casssanisha@gmail.com>
2024-12-27 22:41:07 +0530

    a

---
+++ src/main/java/com/scaler/basicthreads/Conc/example7Mutex/Adder.java
@@ -0,0 +1,23 @@
+package com.scaler.basicthreads.Conc.example7Mutex;
+
+import java.util.concurrent.locks.Lock;
+
+public class Adder implements  Runnable{
+    Count count;
+    Lock lock;
+
+    public Adder(Count count, Lock
+                 lock){
+        this.count=count;
+        this.lock=lock;
+    }
+
+    @Override
+    public void run() {
+        for( int i=0; i<100000; i++ ){
+            lock.lock();
+            this.count.num+=1;
+            lock.unlock();
+        }
+    }
+}
---
+++ src/main/java/com/scaler/basicthreads/Conc/example7Mutex/Count.java
@@ -0,0 +1,5 @@
+package com.scaler.basicthreads.Conc.example7Mutex;
+
+public class Count {
+    int num;
+}
